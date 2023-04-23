05c0f2d6ce961ff25b199dd5f515646b0ddfe5cb
violetmcnally <129994178+violetmcnally@users.noreply.github.com>
2023-04-23 14:51:01 -0700

    wowza

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - rock_paper_scissors
+// Description: This is a implementation of the game rock paper scissors.
+// Author: Violet McNally & Janesse Liang <vmcnally@ucsc.edu> <jlian100@ucsc.edu>
+// Date: April 23rd, 2023
 
 // Constants
 
@@ -19,3 +20,19 @@ function main() {
 
 // let's get this party started
 main();
+      //Define both players and game rules
+          //computer vs. person
+          //if player chooses rock and computer chooses scissors, player wins
+            //else if computer chooses rock and player chooses scissors, computer wins
+          //If player chooses scissors and computer chooses paper, player wins
+            //else if computer chooses scissors and player chooses paper, computer wins
+          //If player chooses paper and computer chooses rock, player wins
+            //else if computer chooses paper and player chooses rock, computer wins
+          //If both player and computer choose rock, they both tie
+          //If both player and computer choose scissors, they both tie
+          //If both player and computer choose paper, they both tie
+      //Player inputs their choice
+           //computer randomly picks rock, paper, or scissors
+      //Delay for 1 second before both choices are revealed
+           //If tie, no points are rewarded
+      //Winner gets announced 
