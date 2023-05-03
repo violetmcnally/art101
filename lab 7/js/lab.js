27bfc665e6b7d748ad15f502de79b037ed74e14a
violetmcnally <129994178+violetmcnally@users.noreply.github.com>
2023-05-03 12:59:03 -0700

    yayyayyyyy

--- lab 7/js/lab.js
+++ lab 7/js/lab.js
@@ -7,16 +7,23 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-    // some code here
-    // return results;
+function sortUserName() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    //above could have been a sinle line
+    //    userName.toLower().split("").sort().join("")
+    return nameSorted;
   }
   
-  function main() {
-    console.log("Main function started.");
-    // the code that makes everything happen
-  }
-  
-  // let's get this party started
-  main();
-  
+//output
+document.writeln("Oh hey, I've fixed your name: ",
+    sortUserName(), "</br>");
