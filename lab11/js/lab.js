24327d59fb9ca86568a73e058a85c347b8f1aa64
violetmcnally <129994178+violetmcnally@users.noreply.github.com>
2023-05-18 09:25:16 -0700

    fixin

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,15 +2,19 @@
  * Author:    Violet McNally <vmcnally@ucsc.edu>
  * Created:   05.16.2023
  * 
- * (c) Copyright by Blub Corp.
+ * (c) Copyright by me!
  **/
 
 
 buttonEl= $('.minor-section').append('<button id=buttonChalEL> click me.</button>');
 
-
-
   buttonEl= $('#buttonChalEL').click(function(){
     $('#challenge').toggleClass('special');
    alert("alert me motherfucker!")
   });
+
+  $(".highlight-button").click(function(){
+    $(this).parent().toggleClass("special");
+  })
+
+  $(".sectionbox").append("<button class= 'btn-secondary hilight button'")("<toggle highlight>")
