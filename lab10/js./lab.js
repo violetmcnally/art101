c5c998414448fa09d1a36fe8d1ff90f67153e5e0
violetmcnally <129994178+violetmcnally@users.noreply.github.com>
2023-05-25 17:49:34 -0700

    rawr

--- lab10/js./lab.js
+++ lab10/js./lab.js
@@ -2,19 +2,24 @@
 // Author: Violet McNally <vmcnally@ucsc.edu>
 // Created: May 15th
 // License: Public Domanin
+
 function sortUserName() {
   var userName = document.getElementById('user-name').value;
-    console.log("userName =", userName);
-    var sortedName = userName.toLowerCase().split('').sort().join('');
-    console.log("userSorted =", userSorted);
-    return nameSorted;
-  }
+  console.log("userName: ", userName);
+  var sortedName = userName.toLowerCase().split('').sort().join('');
+  console.log("sortedName: ", sortedName);
+  return sortedName;
+}
 
 var button = document.getElementById('my-button');
-button.addEventListener('click', function() {
-    inputValue = document.getElementById('my-input').value;
-    console.log("You input:", inputValue);
-    document.writeln("Here you go!", sortUserName(),"</br>")
+var outputEl = document.getElementById('output');
+
+button.addEventListener('click', function () {
+  inputValue = document.getElementById('user-name').value;
+  console.log("Your input:", sortedName);
+  var sortedName = sortUserName();
+  outputEl.innerHTML = "Your sorted name is: " + sortedName;
 });
+
 
 
